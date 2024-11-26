dd1bc3b6a1d303623f4f7255714729344ee10ce6
blocsu <dotsenko.sar@gmail.com>
2024-11-26 22:11:02 +0400

    Работа с файловой системой

--- weather.js
+++ weather.js
@@ -1,7 +1,16 @@
 #!/user/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { printError, printHelp, printSuccess } from './services/log.service.js';
 import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Токен сохранён');
+	} catch (e) {
+		printError(e.message);
+	}
+}
 
 const initCLI = () => {
 	const args = getArgs(process.argv);
@@ -14,7 +23,7 @@ const initCLI = () => {
 		// Сохранить город
 	}
 	if (args.t) {
-		saveKeyValue('token', args.t)
+		return saveToken(args.t);
 	}
 	// Вывести погоду
 }
