e6d71c844e73dff6fc80ec2c56c493ca041c616f
blocsu <dotsenko.sar@gmail.com>
2024-11-26 20:28:18 +0400

    OS and path

--- weather.js
+++ weather.js
@@ -1,17 +1,20 @@
 #!/user/bin/env node
 import { getArgs } from './helpers/args.js';
+import { printHelp } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
 
 const initCLI = () => {
 	const args = getArgs(process.argv);
-	console.log(args);
+	// console.log(args);
 	if (args.h) {
 		//Вывод help
+		printHelp();
 	}	
 	if (args.s) {
 		// Сохранить город
 	}
 	if (args.t) {
-		// Сохранить токен
+		saveKeyValue('token', args.t)
 	}
 	// Вывести погоду
 }
