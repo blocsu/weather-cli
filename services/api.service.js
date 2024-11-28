a7f61130d74bf7d89767c93b2e38a38968e3d26e
blocsu <dotsenko.sar@gmail.com>
2024-11-28 22:19:25 +0400

    Finish

--- services/api.service.js
+++ services/api.service.js
@@ -1,6 +1,29 @@
 import axios from 'axios';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
+const getIcon = (icon) => {
+	switch(icon.slice(0, -1)) {
+		case '01':
+			return '☀️';
+		case '02':
+			return '🌤️';
+		case '03':
+			return '☁️';
+		case '04':
+			return '☁️';
+		case '09':
+			return '🌧️';
+		case '10':
+			return '🌦️';
+		case '11':
+			return '🌩️';
+		case '13':
+			return '❄️';
+		case '50':
+			return '🌫️';
+	}
+}
+
 const getWeather = async (city) => {
 	const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
 	if (!token) {
@@ -15,7 +38,7 @@ const getWeather = async (city) => {
 			units: 'metric'
 		}
 	});
-	console.log(data);	
+	// console.log(data);	
 	return data;
 	
 	// // const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
@@ -36,4 +59,5 @@ const getWeather = async (city) => {
 	// });
 };
 
-export { getWeather };
+export { getIcon, getWeather };
+
--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 #!/user/bin/env node
 import { getArgs } from './helpers/args.js';
-import { getWeather } from './services/api.service.js';
-import { printError, printHelp, printSuccess } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getIcon, getWeather } from './services/api.service.js';
+import { printError, printHelp, printSuccess, printWeather } from './services/log.service.js';
+import { getKeyValue, saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
 	if (!token.length) {
@@ -17,10 +17,24 @@ const saveToken = async (token) => {
 	}
 }
 
+const saveCity = async (city) => {
+	if (!city.length) {
+		printError('Не передан город');
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city);
+		printSuccess('Город сохранён');
+	} catch (e) {
+		printError(e.message);
+	}
+}
+
 const getForcast = async () => {
 	try {
-		const weather = await getWeather(process.env.CITY);
-		console.log(weather);		//Красивый вывод погоды
+		const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+		const weather = await getWeather(city);
+		printWeather(weather, getIcon(weather.weather[0].icon));
 	} catch (e) {
 		if (e?.response?.status == 404) {
 			printError('Неверно указан город');
@@ -29,8 +43,7 @@ const getForcast = async () => {
 		} else {
 			printError(e.message);
 		}
-	}
-	
+	}	
 }
 
 const initCLI = () => {
@@ -40,16 +53,17 @@ const initCLI = () => {
 	// console.log(args);
 	if (args.h) {
 		//Вывод help
-		printHelp();
+		return printHelp();
 	}	
 	if (args.s) {
 		// Сохранить город
+		return saveCity(args.s);
 	}
 	if (args.t) {
 		return saveToken(args.t);
 	}
 	// Вывести погоду
-	getForcast();
+	return getForcast();
 }
 
 initCLI();
